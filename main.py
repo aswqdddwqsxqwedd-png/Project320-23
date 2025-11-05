a0cbc0c1f82e410f02129a852d152e23170dc7fa
soliyev23 <asoliyev53@gmail.com>
2025-11-05 17:48:28 +0500

    I'm Soliyev

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from functions import konstantin
 from functionInoyatov import inoyatov
 from functionilyasik import ilyas
+import function_soliyev as s
 
 print(inoyatov(25,5))
 print(konstantin(3,6))
 print(ilyas(3,4))
+print(s.function_soliyev(4,5))
